368ac597f09d9101283608a6fff892f38867a097
DoNiFF <prodoniff@gmail.com>
2024-09-07 14:34:06 +0300

    Packeges changes: deleting trash, installing new packages

--- BookLibrary.Application/obj/Debug/net8.0/BookLibrary.Application.AssemblyInfo.cs
+++ BookLibrary.Application/obj/Debug/net8.0/BookLibrary.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookLibrary.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+269cc45fbe572849550533d07812d0ee4f0939e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookLibrary.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookLibrary.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ BookLibrary.Database/BookLibraryDBContext.cs
@@ -0,0 +1,17 @@
+﻿using BookLibrary.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookLibrary.Domain
+{
+    public class BookLibraryDBContext : DbContext
+    {
+        public BookLibraryDBContext(DbContextOptions<BookLibraryDBContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<BookCategoryEntity> BookCategories { get; set; }
+        public DbSet<BookEntity> Books { get; set; }
+    }
+}
