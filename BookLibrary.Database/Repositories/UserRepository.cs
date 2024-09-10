196bdab18ce67881ba4363a3b1a1607e6e9a23e7
DoNiFF <prodoniff@gmail.com>
2024-09-10 16:17:51 +0300

    Create the repository for User

---
+++ BookLibrary.Database/Repositories/UserRepository.cs
@@ -0,0 +1,24 @@
+﻿using BookLibrary.Core.Models;
+using BookLibrary.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookLibrary.Database.Repositories;
+
+public class UserRepository
+{
+    private readonly BookLibraryDBContext _dbContext;
+
+    public UserRepository(BookLibraryDBContext dbContext)
+    {
+        _dbContext = dbContext; 
+    }
+
+    public async Task<List<User>> Get()
+    {
+        return await _dbContext.Users
+            .Take(1)
+            .AsNoTracking()
+            .ToListAsync();
+    }
+}
+
